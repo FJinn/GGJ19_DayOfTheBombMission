e8306b3c8954ee1659f2de845b82ddfe37e2e3d9
FJinn <0123433@kdu-online.com>
2019-01-26 09:00:59 +0800

    Some tile information.

--- GGJ19_DayOfTheBombMission/Assets/Script/Tile.cs
+++ GGJ19_DayOfTheBombMission/Assets/Script/Tile.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public enum Direction
     {
-        
+        UP_LEFT,
+        UP_DOWN,
+        DOWN_LEFT,
+        DOWN_RIGHT,
+        LEFT_UP,
+        LEFT_DOWN,
+        RIGHT_UP,
+        RIGHT_DOWN
     }
 
-    // Update is called once per frame
-    void Update()
+    Vector3[] landPositions = new Vector3[8];
+
+    private void Awake()
     {
-        
+        InitializeLandPosition();
+    }
+
+    void InitializeLandPosition()
+    {
+        landPositions[(int)Direction.UP_LEFT] = new Vector3(transform.position.x - transform.localScale.x / 4, transform.position.y, transform.position.z + transform.localScale.z / 2);
+        landPositions[(int)Direction.UP_DOWN] = new Vector3(transform.position.x + transform.localScale.x / 4, transform.position.y, transform.position.z + transform.localScale.z / 2);
+        landPositions[(int)Direction.DOWN_LEFT] = new Vector3(transform.position.x - transform.localScale.x / 4, transform.position.y, transform.position.z - transform.localScale.z / 2);
+        landPositions[(int)Direction.DOWN_RIGHT] = new Vector3(transform.position.x + transform.localScale.x / 4, transform.position.y, transform.position.z - transform.localScale.z / 2);
+        landPositions[(int)Direction.LEFT_UP] = new Vector3(transform.position.x - transform.localScale.x / 2, transform.position.y, transform.position.z + transform.localScale.z / 4);
+        landPositions[(int)Direction.LEFT_DOWN] = new Vector3(transform.position.x - transform.localScale.x / 2, transform.position.y, transform.position.z - transform.localScale.z / 4);
+        landPositions[(int)Direction.RIGHT_UP] = new Vector3(transform.position.x - transform.localScale.x / 2, transform.position.y, transform.position.z + transform.localScale.z / 4);
+        landPositions[(int)Direction.RIGHT_DOWN] = new Vector3(transform.position.x - transform.localScale.x / 2, transform.position.y, transform.position.z - transform.localScale.z / 4);
     }
 }
