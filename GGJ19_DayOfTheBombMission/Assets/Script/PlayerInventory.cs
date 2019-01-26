332e03313eaa5aca43a01c575730a2f8b9b00b6b
Wakakaka00 <jiawei9811@gmail.com>
2019-01-26 10:32:03 +0900

    PlayerInventory

---
+++ GGJ19_DayOfTheBombMission/Assets/Script/PlayerInventory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public List<Tile> tileList = new List<Tile>();
+
+    private Tile selectedTile;
+
+    int num = 0;
+
+    private void Start()
+    {
+        selectedTile = tileList[num];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChooseTile();
+    }
+
+
+
+    void ChooseTile()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (num == 2)
+            {
+                selectedTile = tileList[0];
+            }
+            else
+            {
+                selectedTile = tileList[num + 1];
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (num == 0)
+            {
+                selectedTile = tileList[2];
+            }
+            else
+            {
+                selectedTile = tileList[num - 1];
+            }
+        }
+    }
+}
