98def30766ab97ec22c8d6f3e97a5c6f966961a5
FJinn <0123433@kdu-online.com>
2019-01-26 10:25:28 +0800

    MORE MORE tile information.

--- GGJ19_DayOfTheBombMission/Assets/Script/Tile.cs
+++ GGJ19_DayOfTheBombMission/Assets/Script/Tile.cs
@@ -7,7 +7,7 @@ public class Tile : MonoBehaviour
     public enum Direction
     {
         TOP_LEFT,
-        TOP_DOWN,
+        TOP_RIGHT,
         BOTTOM_LEFT,
         BOTTOM_RIGHT,
         LEFT_TOP,
@@ -16,11 +16,23 @@ public class Tile : MonoBehaviour
         RIGHT_BOTTOM
     }
 
+    public int patternID;
+
+    [SerializeField] Vector3 currentPosition;
+    /// <summary>
+    /// put currentDirection into nextLandPostisions as index
+    /// </summary>
+    Direction currentDirection;
     Vector3[] landPositions = new Vector3[8];
+    /// <summary>
+    /// index stands for the current position in terms of direction
+    /// </summary>
+    Vector3[] nextLandPositions = new Vector3[8];
     /// <summary>
     /// 0 = top, 1 = left, 2 = down, 3 = right
     /// </summary>
     Tile[] connectedTiles = new Tile[4];
+
 
     private void Awake()
     {
@@ -31,7 +43,7 @@ public class Tile : MonoBehaviour
     void InitializeLandPosition()
     {
         landPositions[(int)Direction.TOP_LEFT] = new Vector3(transform.position.x - transform.localScale.x / 4, transform.position.y, transform.position.z + transform.localScale.z / 2);
-        landPositions[(int)Direction.TOP_DOWN] = new Vector3(transform.position.x + transform.localScale.x / 4, transform.position.y, transform.position.z + transform.localScale.z / 2);
+        landPositions[(int)Direction.TOP_RIGHT] = new Vector3(transform.position.x + transform.localScale.x / 4, transform.position.y, transform.position.z + transform.localScale.z / 2);
         landPositions[(int)Direction.BOTTOM_LEFT] = new Vector3(transform.position.x - transform.localScale.x / 4, transform.position.y, transform.position.z - transform.localScale.z / 2);
         landPositions[(int)Direction.BOTTOM_RIGHT] = new Vector3(transform.position.x + transform.localScale.x / 4, transform.position.y, transform.position.z - transform.localScale.z / 2);
         landPositions[(int)Direction.LEFT_TOP] = new Vector3(transform.position.x - transform.localScale.x / 2, transform.position.y, transform.position.z + transform.localScale.z / 4);
@@ -68,5 +80,188 @@ public class Tile : MonoBehaviour
         connectedTiles[3] = selectedTile;
     }
 
+    /// <summary>
+    /// get direction for nextLandPositions[(int)currentDirection]
+    /// </summary>
+    /// <returns></returns>
+    Direction GetCurrentDirection()
+    {
+        for(int i=0; i<landPositions.Length; i++)
+        {
+            if (currentPosition == landPositions[i])
+            {
+                currentDirection = (Direction)i;
+            }
+        }
 
+        return currentDirection;
+    }
+
+    /// <summary>
+    /// put in the patternID when you call for tile to initialize the pattern
+    /// </summary>
+    /// <param name="patternID"></param>
+    public void InitializePattern(int patternID)
+    {
+        if(patternID == 1)
+        {
+            PatternOne();
+        }
+        else if (patternID == 2)
+        {
+            PatternTwo();
+        }
+        else if (patternID == 3)
+        {
+            PatternThree();
+        }
+        else if (patternID == 4)
+        {
+            PatternFour();
+        }
+        else if (patternID == 5)
+        {
+            PatternFive();
+        }
+        else if (patternID == 6)
+        {
+            PatternSix();
+        }
+        else if (patternID == 7)
+        {
+            PatternSeven();
+        }
+        else if (patternID == 8)
+        {
+            PatternEight();
+        }
+        else if (patternID == 9)
+        {
+            PatternNine();
+        }
+        else if (patternID == 10)
+        {
+            PatternTen();
+        }
+    }
+
+    void PatternOne()
+    {
+        nextLandPositions[(int)Direction.TOP_LEFT] = landPositions[(int)Direction.BOTTOM_LEFT];
+        nextLandPositions[(int)Direction.TOP_RIGHT] = landPositions[(int)Direction.BOTTOM_RIGHT];
+        nextLandPositions[(int)Direction.BOTTOM_LEFT] = landPositions[(int)Direction.TOP_LEFT];
+        nextLandPositions[(int)Direction.BOTTOM_RIGHT] = landPositions[(int)Direction.TOP_RIGHT];
+        nextLandPositions[(int)Direction.LEFT_TOP] = landPositions[(int)Direction.RIGHT_TOP];
+        nextLandPositions[(int)Direction.LEFT_BOTTOM] = landPositions[(int)Direction.RIGHT_BOTTOM];
+        nextLandPositions[(int)Direction.RIGHT_TOP] = landPositions[(int)Direction.LEFT_TOP];
+        nextLandPositions[(int)Direction.RIGHT_BOTTOM] = landPositions[(int)Direction.LEFT_BOTTOM];
+    }
+
+    void PatternTwo()
+    {
+        nextLandPositions[(int)Direction.TOP_LEFT] = landPositions[(int)Direction.LEFT_BOTTOM];
+        nextLandPositions[(int)Direction.TOP_RIGHT] = landPositions[(int)Direction.BOTTOM_LEFT];
+        nextLandPositions[(int)Direction.BOTTOM_LEFT] = landPositions[(int)Direction.TOP_RIGHT];
+        nextLandPositions[(int)Direction.BOTTOM_RIGHT] = landPositions[(int)Direction.RIGHT_TOP];
+        nextLandPositions[(int)Direction.LEFT_TOP] = landPositions[(int)Direction.BOTTOM_RIGHT];
+        nextLandPositions[(int)Direction.LEFT_BOTTOM] = landPositions[(int)Direction.TOP_LEFT];
+        nextLandPositions[(int)Direction.RIGHT_TOP] = landPositions[(int)Direction.BOTTOM_RIGHT];
+        nextLandPositions[(int)Direction.RIGHT_BOTTOM] = landPositions[(int)Direction.LEFT_TOP];
+    }
+
+    void PatternThree()
+    {
+        nextLandPositions[(int)Direction.TOP_LEFT] = landPositions[(int)Direction.RIGHT_TOP];
+        nextLandPositions[(int)Direction.TOP_RIGHT] = landPositions[(int)Direction.LEFT_TOP];
+        nextLandPositions[(int)Direction.BOTTOM_LEFT] = landPositions[(int)Direction.RIGHT_BOTTOM];
+        nextLandPositions[(int)Direction.BOTTOM_RIGHT] = landPositions[(int)Direction.LEFT_BOTTOM];
+        nextLandPositions[(int)Direction.LEFT_TOP] = landPositions[(int)Direction.TOP_RIGHT];
+        nextLandPositions[(int)Direction.LEFT_BOTTOM] = landPositions[(int)Direction.BOTTOM_RIGHT];
+        nextLandPositions[(int)Direction.RIGHT_TOP] = landPositions[(int)Direction.TOP_LEFT];
+        nextLandPositions[(int)Direction.RIGHT_BOTTOM] = landPositions[(int)Direction.BOTTOM_LEFT];
+    }
+
+    void PatternFour()
+    {
+        nextLandPositions[(int)Direction.TOP_LEFT] = landPositions[(int)Direction.TOP_RIGHT];
+        nextLandPositions[(int)Direction.TOP_RIGHT] = landPositions[(int)Direction.TOP_LEFT];
+        nextLandPositions[(int)Direction.BOTTOM_LEFT] = landPositions[(int)Direction.BOTTOM_RIGHT];
+        nextLandPositions[(int)Direction.BOTTOM_RIGHT] = landPositions[(int)Direction.BOTTOM_LEFT];
+        nextLandPositions[(int)Direction.LEFT_TOP] = landPositions[(int)Direction.RIGHT_TOP];
+        nextLandPositions[(int)Direction.LEFT_BOTTOM] = landPositions[(int)Direction.RIGHT_BOTTOM];
+        nextLandPositions[(int)Direction.RIGHT_TOP] = landPositions[(int)Direction.LEFT_TOP];
+        nextLandPositions[(int)Direction.RIGHT_BOTTOM] = landPositions[(int)Direction.LEFT_BOTTOM];
+    }
+    
+    void PatternFive()
+    {
+        nextLandPositions[(int)Direction.TOP_LEFT] = landPositions[(int)Direction.LEFT_TOP];
+        nextLandPositions[(int)Direction.TOP_RIGHT] = landPositions[(int)Direction.RIGHT_TOP];
+        nextLandPositions[(int)Direction.BOTTOM_LEFT] = landPositions[(int)Direction.LEFT_BOTTOM];
+        nextLandPositions[(int)Direction.BOTTOM_RIGHT] = landPositions[(int)Direction.RIGHT_BOTTOM];
+        nextLandPositions[(int)Direction.LEFT_TOP] = landPositions[(int)Direction.TOP_LEFT];
+        nextLandPositions[(int)Direction.LEFT_BOTTOM] = landPositions[(int)Direction.BOTTOM_LEFT];
+        nextLandPositions[(int)Direction.RIGHT_TOP] = landPositions[(int)Direction.TOP_RIGHT];
+        nextLandPositions[(int)Direction.RIGHT_BOTTOM] = landPositions[(int)Direction.BOTTOM_RIGHT];
+    }
+
+    void PatternSix()
+    {
+        nextLandPositions[(int)Direction.TOP_LEFT] = landPositions[(int)Direction.RIGHT_BOTTOM];
+        nextLandPositions[(int)Direction.TOP_RIGHT] = landPositions[(int)Direction.LEFT_BOTTOM];
+        nextLandPositions[(int)Direction.BOTTOM_LEFT] = landPositions[(int)Direction.LEFT_TOP];
+        nextLandPositions[(int)Direction.BOTTOM_RIGHT] = landPositions[(int)Direction.RIGHT_TOP];
+        nextLandPositions[(int)Direction.LEFT_TOP] = landPositions[(int)Direction.BOTTOM_LEFT];
+        nextLandPositions[(int)Direction.LEFT_BOTTOM] = landPositions[(int)Direction.TOP_RIGHT];
+        nextLandPositions[(int)Direction.RIGHT_TOP] = landPositions[(int)Direction.BOTTOM_RIGHT];
+        nextLandPositions[(int)Direction.RIGHT_BOTTOM] = landPositions[(int)Direction.TOP_LEFT];
+    }
+
+    void PatternSeven()
+    {
+        nextLandPositions[(int)Direction.TOP_LEFT] = landPositions[(int)Direction.BOTTOM_RIGHT];
+        nextLandPositions[(int)Direction.TOP_RIGHT] = landPositions[(int)Direction.BOTTOM_LEFT];
+        nextLandPositions[(int)Direction.BOTTOM_LEFT] = landPositions[(int)Direction.TOP_RIGHT];
+        nextLandPositions[(int)Direction.BOTTOM_RIGHT] = landPositions[(int)Direction.TOP_LEFT];
+        nextLandPositions[(int)Direction.LEFT_TOP] = landPositions[(int)Direction.RIGHT_BOTTOM];
+        nextLandPositions[(int)Direction.LEFT_BOTTOM] = landPositions[(int)Direction.RIGHT_TOP];
+        nextLandPositions[(int)Direction.RIGHT_TOP] = landPositions[(int)Direction.LEFT_BOTTOM];
+        nextLandPositions[(int)Direction.RIGHT_BOTTOM] = landPositions[(int)Direction.LEFT_TOP];
+    }
+
+    void PatternEight()
+    {
+        nextLandPositions[(int)Direction.TOP_LEFT] = landPositions[(int)Direction.LEFT_TOP];
+        nextLandPositions[(int)Direction.TOP_RIGHT] = landPositions[(int)Direction.LEFT_BOTTOM];
+        nextLandPositions[(int)Direction.BOTTOM_LEFT] = landPositions[(int)Direction.RIGHT_TOP];
+        nextLandPositions[(int)Direction.BOTTOM_RIGHT] = landPositions[(int)Direction.RIGHT_BOTTOM];
+        nextLandPositions[(int)Direction.LEFT_TOP] = landPositions[(int)Direction.TOP_LEFT];
+        nextLandPositions[(int)Direction.LEFT_BOTTOM] = landPositions[(int)Direction.TOP_RIGHT];
+        nextLandPositions[(int)Direction.RIGHT_TOP] = landPositions[(int)Direction.BOTTOM_LEFT];
+        nextLandPositions[(int)Direction.RIGHT_BOTTOM] = landPositions[(int)Direction.BOTTOM_RIGHT];
+    }
+
+    void PatternNine()
+    {
+        nextLandPositions[(int)Direction.TOP_LEFT] = landPositions[(int)Direction.RIGHT_TOP];
+        nextLandPositions[(int)Direction.TOP_RIGHT] = landPositions[(int)Direction.BOTTOM_LEFT];
+        nextLandPositions[(int)Direction.BOTTOM_LEFT] = landPositions[(int)Direction.TOP_RIGHT];
+        nextLandPositions[(int)Direction.BOTTOM_RIGHT] = landPositions[(int)Direction.LEFT_TOP];
+        nextLandPositions[(int)Direction.LEFT_TOP] = landPositions[(int)Direction.BOTTOM_RIGHT];
+        nextLandPositions[(int)Direction.LEFT_BOTTOM] = landPositions[(int)Direction.RIGHT_BOTTOM];
+        nextLandPositions[(int)Direction.RIGHT_TOP] = landPositions[(int)Direction.TOP_LEFT];
+        nextLandPositions[(int)Direction.RIGHT_BOTTOM] = landPositions[(int)Direction.LEFT_BOTTOM];
+    }
+
+    void PatternTen()
+    {
+        nextLandPositions[(int)Direction.TOP_LEFT] = landPositions[(int)Direction.RIGHT_BOTTOM];
+        nextLandPositions[(int)Direction.TOP_RIGHT] = landPositions[(int)Direction.LEFT_BOTTOM];
+        nextLandPositions[(int)Direction.BOTTOM_LEFT] = landPositions[(int)Direction.RIGHT_TOP];
+        nextLandPositions[(int)Direction.BOTTOM_RIGHT] = landPositions[(int)Direction.LEFT_TOP];
+        nextLandPositions[(int)Direction.LEFT_TOP] = landPositions[(int)Direction.BOTTOM_RIGHT];
+        nextLandPositions[(int)Direction.LEFT_BOTTOM] = landPositions[(int)Direction.TOP_RIGHT];
+        nextLandPositions[(int)Direction.RIGHT_TOP] = landPositions[(int)Direction.BOTTOM_LEFT];
+        nextLandPositions[(int)Direction.RIGHT_BOTTOM] = landPositions[(int)Direction.TOP_LEFT];
+    }
 }
